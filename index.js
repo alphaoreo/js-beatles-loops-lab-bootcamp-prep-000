b224a76620ea965ab0600a2e28e2fa4f55853f12
algebraic-acrobat-1174 <mayowaa4@gmail.com>
2020-07-15 15:27:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(artists, instruments){
+  var result= []
+  for (var i = 0; i < artists.length; i++) {
+    result[i] = `${artists[i]} plays ${instruments[i]}`
+  }
+  return result
+}
